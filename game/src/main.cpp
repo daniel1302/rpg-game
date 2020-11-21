dd5c43e9c38ad2d70dca971545d5b1c048a09b98
Daniel Hornik <daniel.1302@gmail.com>
2020-11-21 21:11:25 +0100

    Fix emscripten loop

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,147 +1,26 @@
+#include <emscripten.h>
+#include <emscripten/html5.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-#if __EMSCRIPTEN__
-#include <emscripten/emscripten.h>
-#include <SDL.h>
-#include <SDL_ttf.h>
-#else
-#include <SDL.h>
-#include <SDL_ttf.h>
-#endif
-
-#define SCREEN_WIDTH  200
-#define SCREEN_HEIGHT 200
-
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-
-#define MAX(a,b) ((a) > (b) ? a : b)
-#define MIN(a,b) ((a) < (b) ? a : b)
-
-int posX=0;
-int posY=0;
-int sizeW=20;
-int sizeH=20;
-
-static int quit = 0;
-
-void render()
-{
-
-    SDL_Rect r_scr;
-    r_scr.x = posX;
-    r_scr.y = posY;
-    r_scr.w = sizeW;
-    r_scr.h = sizeH;
-    SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0x00, 0xFF );
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0x00, 0x00);
-    SDL_RenderDrawRect(renderer, &r_scr);
-    SDL_RenderPresent(renderer);
+// Our "main loop" function. This callback receives the current time as
+// reported by the browser, and the user data we provide in the call to
+// emscripten_request_animation_frame_loop().
+EM_BOOL one_iter(double time, void* userData) {
+  // Can render to the screen here, etc.
+  puts("one iteration");
+  // Return true to keep the loop running.
+  return EM_TRUE;
 }
 
-#if __EMSCRIPTEN__
-void main_tick() {
+int main() {
+#ifdef __EMSCRIPTEN__
+  // Receives a function to call and some user data to provide it.
+  emscripten_request_animation_frame_loop(one_iter, 0);
 #else
-int main_tick() {
+  while (1) {
+    one_iter();
+    // Delay to keep frame rate constant (using SDL).
+    SDL_Delay(time_to_next_frame());
+  }
 #endif
-
-    SDL_Event event;
-
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-            {
-                quit = 1;
-                break;
-            }
-            case SDL_KEYDOWN:
-            {
-                switch (event.key.keysym.sym)
-                {
-                case SDLK_UP:
-                {
-                    if (posY>=20)
-                    {
-                        posY-=20;
-                    }
-                    break;
-                }
-                case SDLK_DOWN:
-                {
-                    if (posY+sizeH<SCREEN_HEIGHT)
-                    {
-                        posY += 20;
-                    }
-                    break;
-                }
-                case SDLK_LEFT:
-                {
-                    if (posX>=20)
-                    {
-                        posX-=20;
-                    }
-                    break;
-                }
-                case SDLK_RIGHT:
-                {
-                    if (posX+sizeW<SCREEN_WIDTH)
-                    {
-                        posX+=20;
-                    }
-                    break;
-                }
-                }
-                break;
-            }
-            }
-
-        }
-
-
-    render();
-    SDL_UpdateWindowSurface(window);
-
-#if !__EMSCRIPTEN__
-    return 0;
-#endif
-}
-
-void main_loop()
-{
-
-#if __EMSCRIPTEN__
-    emscripten_set_main_loop(main_tick, -1, 1);
-#else
-    while (0 == quit)
-    {
-        main_tick();
-    }
-#endif
-}
-
-int main()
-{
-    SDL_Init(SDL_INIT_VIDEO);
-
-    window = SDL_CreateWindow(
-        "WEBASM",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH, SCREEN_HEIGHT,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-
-    main_loop();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
 }
