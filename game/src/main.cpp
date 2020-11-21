689e1640af2478c09c42cee6e1a4faf383ee3364
Daniel Hornik <daniel.1302@gmail.com>
2020-11-21 21:44:32 +0100

    Fix emscripten loop

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,32 +1,6 @@
-#include <emscripten.h>
-#include <emscripten/html5.h>
 #include <stdio.h>
 
-// Our "main loop" function. This callback receives the current time as
-// reported by the browser, and the user data we provide in the call to
-// emscripten_request_animation_frame_loop().
-EM_BOOL one_iter() {
-  // Can render to the screen here, etc.
-  puts("one iteration");
-  // Return true to keep the loop running.
-  return EM_TRUE;
-}
-
-void runEmscriptenMainLoop()
-{
-    one_iter();
-}
-int main() {
-
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(runEmscriptenMainLoop, 60, 1);
-
-#else
-    while (runMainLoop())
-    {
-        one_iter();
-        // Delay to keep frame rate constant (using SDL).
-        SDL_Delay(time_to_next_frame());
-    }
-#endif
+int main(){
+    printf("Hello world!\n");
+    return 0;
 }
